a82cee0a05dbd6c4e9e34860061bfa2e4c747d4d
AlonsoMartinToledano <alonsomtgm@gmail.com>
2020-02-28 15:15:14 +0100

    Click result

--- calculator-react/src/App.jsx
+++ calculator-react/src/App.jsx
@@ -22,30 +22,28 @@ class App extends Component {
     result[1] = result[0];
 
     if (result[0].includes("sin(")) {
-      result[0] = result[0].replace("sin(", '');
-      result[0] = result[0].replace(")", "");
-      result[0] = Math.sin(result[0]);
+      result[0] = result[0].replace("sin(", "Math.sin(");
     }
     else if (result[0].includes("cos(")) {
-      result[0] = result[0].replace("cos(", '');
-      result[0] = result[0].replace(")", "");
-      result[0] = Math.cos(result[0]);
+      result[0] = result[0].replace("cos(", "Math.cos(");
     }
     else if (result[0].includes("tan(")) {
-      result[0] = result[0].replace("tan(", '');
-      result[0] = result[0].replace(")", "");
-      result[0] = Math.tan(result[0]);
-    }
-    else {
-      result[0] = eval(this.state.result[0]);
+      result[0] = result[0].replace("tan(", "Math.tan(");
     }
 
+    result[0] = eval(this.state.result[0]);
     this.setState({result});
   }
 
   onCHandler = () => {
     const result = this.state.result;
     result[0] = "";
+    this.setState({result});
+  }
+
+  onResultHandler = (index) => {
+    const result = this.state.result;
+    result[0] = result[index];
     this.setState({result});
   }
 
@@ -64,7 +62,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculator data={this.state} onClick={{onButton: this.onButtonHandler, onEqual: this.onEqualHandler, onC: this.onCHandler, onDelete: this.onDeleteHandler, onMode: this.onModeHandler}}/>
+        <Calculator data={this.state} onClick={{onButton: this.onButtonHandler, onEqual: this.onEqualHandler, onC: this.onCHandler, onDelete: this.onDeleteHandler, onMode: this.onModeHandler, onResult: this.onResultHandler}}/>
       </div>
     );
   }
--- calculator-react/src/components/Header.jsx
+++ calculator-react/src/components/Header.jsx
@@ -3,13 +3,13 @@ import "./styles.css";
 
 const Header = (props) => {
     const {data} = props;
-    const {onMode} = props.onClick;
+    const {onMode, onResult} = props.onClick;
     return (
         <div className="Header">
             <div className="ChangeButton" onClick={onMode}>Mode</div>
                 <div className="Display">
-                    <div className="Result">{data.result[2]}</div>
-                    <div className="Result">{data.result[1]}</div>
+                    <div className="Result" onClick={ () => onResult(2) }>{data.result[2]}</div>
+                    <div className="Result" onClick={ () => onResult(1) }>{data.result[1]}</div>
                     <div className="Operation">{data.result[0]}</div>
                 </div>
         </div>       
